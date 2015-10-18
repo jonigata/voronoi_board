3e717b4e4f23e4b536e086fb95621bd428903523
Naoyuki Hirayama <naoyuki.hirayama@gmail.com>
2015-10-18 17:57:59 +0900

    add too-close-sites tier

--- TacticsBoard.hx
+++ TacticsBoard.hx
@@ -10,6 +10,7 @@ class TacticsBoard {
         for (i in 0...100) {
             sites.push({x: Math.random() * 960, y: Math.random() * 640});
         }
+        tieClosest();
         
         diagram = new Voronoi().compute(
             sites, {xl: 0, xr: 960, yt: 0, yb: 640});
@@ -30,6 +31,48 @@ class TacticsBoard {
                 }
                 ctx.stroke();
             }
+        }
+    }
+
+    function tieClosest() {
+        function distance(a: Site, b: Site) {
+            var dx = a.x - b.x;
+            var dy = a.y - b.y;
+            return Math.sqrt(dx*dx+dy*dy);
+        }
+
+        while(true) {
+            // calculate nearest
+            var nearestDistance = 99999.0;
+            var nearestA = -1;
+            var nearestB = -1;
+            for (i in 0...sites.length) {
+                for (j in (i+1)...sites.length) {
+                    var d = distance(sites[i], sites[j]);
+                    if (d < nearestDistance) {
+                        nearestDistance = d;
+                        nearestA = i;
+                        nearestB = j;
+                    }
+                }
+            }
+
+            if (100.0 < nearestDistance) {
+                break;
+            }
+
+            // tie
+            var newSites = [];
+            for (i in 0...sites.length) {
+                if (i == nearestA || i == nearestB) {
+                } else {
+                    newSites.push(sites[i]);
+                }
+            }
+            var a = sites[nearestA];
+            var b = sites[nearestB];
+            newSites.push({x: (a.x + b.x) * 0.5, y: (a.y + b.y) * 0.5});
+            sites = newSites;
         }
     }
 }
